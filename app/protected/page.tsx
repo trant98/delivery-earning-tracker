b585daceaeed4d8f2ff6239e20d1da508f1794a3
Tri-T <tranmaitri.244@gmail.com>
2026-05-25 11:54:20 -0700

    Add EarningsTable to the Dashboard page and display it as raw JSON string

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,43 +1,65 @@
-import { redirect } from "next/navigation";
-
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 import { Suspense } from "react";
 
-async function UserDetails() {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getClaims();
+async function EarningsTable() {
+  let content;  // return var
 
-  if (error || !data?.claims) {
-    redirect("/auth/login");
+  // connect to supabase => get a supabase object
+  const supabase = await createClient();
+
+  // go to "earnings" table from the supabase object
+  // and select all the columns
+  const { data: earnings, error } = await supabase.from("earnings").select("*");
+
+  // if error, show error
+  if (error) {
+    content = <p>Error: Cannot load earnings</p>
+
+    // else if empty, show "no earnings yet"
+  } else if (earnings.length === 0) {
+    content = <p>No earnings yet. Please add your first entry</p>
+
+    // else show table of earnings
+  } else {
+    content = <pre>{JSON.stringify(earnings, null, 2)}</pre>
   }
 
-  return JSON.stringify(data.claims, null, 2);
+  return content;
 }
 
 export default function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
+
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
+          This is a protected page that you can only see as an authenticated user
         </div>
       </div>
+
+      <div className="flex flex-col gap-2 items-start">
+        <h2 className="font-bold text-2xl mb-4">Earnings Table</h2>
+        <Suspense>
+          <EarningsTable />
+        </Suspense>
+      </div>
+
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
+        {/* <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           <Suspense>
             <UserDetails />
           </Suspense>
-        </pre>
+        </pre> */}
       </div>
+
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
+        {/* <FetchDataSteps /> */}
       </div>
+
     </div>
   );
 }
