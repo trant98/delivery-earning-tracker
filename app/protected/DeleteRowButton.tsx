3ec266ab5150bf146a1434c9e663dd17873ef928
Tri-T <tranmaitri.244@gmail.com>
2026-05-30 13:30:20 -0700

    Users can DELETE an entry now

--- app/protected/DeleteRowButton.tsx
+++ app/protected/DeleteRowButton.tsx
@@ -1,11 +1,8 @@
 "use client";   // Client Component
-import { useState } from "react";
 import { createClient } from "@/lib/supabase/client";  // Client Cmponent
 import { useRouter } from "next/navigation";    // to refresh page after delete
 
 export default function DeleteRowButton({ delete_id }: { delete_id: number }) {
-    // delete message to show result of delete action
-    const [delete_msg, setDeleteMsg] = useState("");
     const router = useRouter();  // to refresh page after delete
 
     async function handleDelete() {
@@ -17,21 +14,13 @@ export default function DeleteRowButton({ delete_id }: { delete_id: number }) {
 
         // if error, show error message
         if (error) {
-            setDeleteMsg(error.message);
+            console.log(error);
 
             // else, show success message
         } else {
-            setDeleteMsg("Your entry has been removed")
             router.refresh();   // refresh router
         }
-
     }
 
-    return (
-        <div>
-            <button type="button" onClick={handleDelete}>Delete</button>
-
-            {delete_msg}
-        </div>
-    );
+    return (<button type="button" onClick={handleDelete}>Delete</button>);
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -24,7 +24,15 @@ async function EarningsTable() {
 
     // else show table of earnings
   } else {
-    content = <pre>{JSON.stringify(earnings, null, 2)}</pre>
+    content = earnings.map((entry) => {
+      return <div key={entry.id}>
+        {entry.date}
+        {entry.source}
+        {entry.income}
+        {entry.mileage}
+        <DeleteRowButton delete_id={entry.id} />
+      </div>;
+    })
   }
 
   return content;
@@ -48,20 +56,6 @@ export default function ProtectedPage() {
         <Suspense>
           <EarningsTable />
         </Suspense>
-      </div>
-
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        {/* <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          <Suspense>
-            <UserDetails />
-          </Suspense>
-        </pre> */}
-      </div>
-
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        {/* <FetchDataSteps /> */}
       </div>
 
     </div>
